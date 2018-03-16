d3e5a2751bf30eb2010818eee1454eb4420d40c7
webdesky <webdeskytechnical@gmail.com>
2018-03-16 15:52:18 +0530

    css changes

--- application/modules/admin/views/setting.php
+++ application/modules/admin/views/setting.php
@@ -21,105 +21,119 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12 col-md-12">
-                            <form role="form" method="post" action="<?php echo base_url('admin/setting') ?>" class="registration_form1" enctype="multipart/form-data">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">English Site Title * </label>
-                                        <div class="col-md-9">
-                                            <input type="text" id="en_site_title" name="en_site_title" class="form-control" value="<?php echo  $setting[0]['en_site_title']?>">
-                                            <span class="red"><?php echo form_error('en_site_title'); ?></span> </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">Hindi Site Title * </label>
-                                        <div class="col-md-9">
-                                            <input type="text" id="hi_site_title" name="hi_site_title" class="form-control" value="<?php echo  $setting[0]['hi_site_title']?>">
-                                            <span class="red"><?php echo form_error('hi_site_title'); ?></span> </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">English Meta Tags * </label>
-                                        <div class="col-md-9">
-                                            <input type="text" id="en_meta_tags" name="en_meta_tags" class="form-control" value="<?php echo  $setting[0]['en_meta_tags']?>">
-                                            <input type="hidden" id="id" name="id" class="form-control" value="<?php echo  $setting[0]['id']?>">
-                                            <span class="red"><?php echo form_error('en_meta_tags'); ?></span> </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">Hindi Meta Tags * </label>
-                                        <div class="col-md-9">
-                                            <input type="text" id="hi_meta_tags" name="hi_meta_tags" class="form-control" value="<?php echo  $setting[0]['hi_meta_tags']?>">
-                                            <span class="red"><?php echo form_error('hi_meta_tags'); ?></span> </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">English CopyRight * </label>
-                                        <div class="col-md-9">
-                                            <textarea class="form-control" rows="5" id="en_copyright" name="en_copyright" placeholder="copyright"><?php echo  $setting[0]['en_copyright']?>
-                                        </textarea> <span class="red"><?php echo form_error('en_copyright'); ?></span>
-                                            <script type="text/javascript">
-                                                CKEDITOR.replace('en_copyright');
-                                            </script>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">Hindi CopyRight * </label>
-                                        <div class="col-md-9">
-                                            <textarea class="form-control" rows="5" id="hi_copyright" name="hi_copyright" placeholder="copyright"><?php echo  $setting[0]['hi_copyright']?>
-                                        </textarea> <span class="red"><?php echo form_error('hi_copyright'); ?></span>
-                                            <script type="text/javascript">
-                                                CKEDITOR.replace('hi_copyright');
-                                            </script>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">English Contact Us * </label>
-                                        <div class="col-md-9">
-                                            <textarea class="form-control" rows="5" id="en_contact_us" name="en_contact_us" placeholder="contact_us"><?php echo  $setting[0]['en_contact_us']?>
-                                        </textarea> <span class="red"><?php echo form_error('en_contact_us'); ?></span>
-                                            <script type="text/javascript">
-                                                CKEDITOR.replace('en_contact_us');
-                                            </script>
-                                        </div>
-                                    </div>
-                                </div>
 
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">Hindi  Contact Us * </label>
-                                        <div class="col-md-9">
-                                            <textarea class="form-control" rows="5" id="hi_contact_us" name="hi_contact_us" placeholder="contact_us"><?php echo  $setting[0]['hi_contact_us']?>
-                                        </textarea> <span class="red"><?php echo form_error('hi_contact_us'); ?></span>
-                                            <script type="text/javascript">
-                                                CKEDITOR.replace('hi_contact_us');
-                                            </script>
+                            <form role="form" method="post" action="<?php echo base_url('admin/setting') ?>" class="registration_form1" enctype="multipart/form-data">
+                                <div class="col-md-6 border-shadowss">
+                                    <div class="en_div">English</div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">Site Title * </label>
+                                            <div class="col-md-9">
+                                                <input type="text" id="en_site_title" name="en_site_title" class="form-control" value="<?php echo  $setting[0]['en_site_title']?>">
+                                                <span class="red"><?php echo form_error('en_site_title'); ?></span> </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">Meta Tags * </label>
+                                            <div class="col-md-9">
+                                                <input type="text" id="en_meta_tags" name="en_meta_tags" class="form-control" value="<?php echo  $setting[0]['en_meta_tags']?>">
+                                                <input type="hidden" id="id" name="id" class="form-control" value="<?php echo  $setting[0]['id']?>">
+                                                <span class="red"><?php echo form_error('en_meta_tags'); ?></span> </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">CopyRight * </label>
+                                            <div class="col-md-9">
+                                                <textarea class="form-control" rows="5" id="en_copyright" name="en_copyright" placeholder="copyright"><?php echo  $setting[0]['en_copyright']?>
+                                                </textarea> <span class="red"><?php echo form_error('en_copyright'); ?></span>
+                                                <script type="text/javascript">
+                                                    CKEDITOR.replace('en_copyright');
+                                                </script>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">Contact Us * </label>
+                                            <div class="col-md-9">
+                                                <textarea class="form-control" rows="5" id="en_contact_us" name="en_contact_us" placeholder="contact_us"><?php echo  $setting[0]['en_contact_us']?>
+                                                </textarea> <span class="red"><?php echo form_error('en_contact_us'); ?></span>
+                                                <script type="text/javascript">
+                                                    CKEDITOR.replace('en_contact_us');
+                                                </script>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">Favicon Icon * </label>
+                                            <div class="col-md-9">
+                                                <input type="file" name="favicon_icon" class="form-control">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">Favicon Icon * </label>
-                                        <div class="col-md-9">
-                                            <input type="file" name="favicon_icon" class="form-control">
+                                <div class="col-md-6 border-shadowss">
+                                    <div class="hi_div">Hindi</div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">Site Title * </label>
+                                            <div class="col-md-9">
+                                                <input type="text" id="hi_site_title" name="hi_site_title" class="form-control" value="<?php echo  $setting[0]['hi_site_title']?>">
+                                                <span class="red"><?php echo form_error('hi_site_title'); ?></span> </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">Meta Tags * </label>
+                                            <div class="col-md-9">
+                                                <input type="text" id="hi_meta_tags" name="hi_meta_tags" class="form-control" value="<?php echo  $setting[0]['hi_meta_tags']?>">
+                                                <span class="red"><?php echo form_error('hi_meta_tags'); ?></span> </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">CopyRight * </label>
+                                            <div class="col-md-9">
+                                                <textarea class="form-control" rows="5" id="hi_copyright" name="hi_copyright" placeholder="copyright"><?php echo  $setting[0]['hi_copyright']?>
+                                                </textarea> <span class="red"><?php echo form_error('hi_copyright'); ?></span>
+                                                <script type="text/javascript">
+                                                    CKEDITOR.replace('hi_copyright');
+                                                </script>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">Contact Us * </label>
+                                            <div class="col-md-9">
+                                                <textarea class="form-control" rows="5" id="hi_contact_us" name="hi_contact_us" placeholder="contact_us"><?php echo  $setting[0]['hi_contact_us']?>
+                                                </textarea> <span class="red"><?php echo form_error('hi_contact_us'); ?></span>
+                                                <script type="text/javascript">
+                                                    CKEDITOR.replace('hi_contact_us');
+                                                </script>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="col-md-3">Site Logo * </label>
+                                            <div class="col-md-9">
+                                                <input type="file" name="site_logo" class="form-control">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="col-md-3">Site Logo * </label>
-                                        <div class="col-md-9">
-                                            <input type="file" name="site_logo" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
+                                <div class="clearfix"></div>
+                                <br><br>
                                 <div class="col-md-12" align="center">
                                     <button type="submit" value="Save" class="btn btn-success">Save</button>
                                     <input type="reset" class="btn btn-default" value="Reset">
@@ -139,16 +153,17 @@
 </div>
 </div>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('select').niceSelect();
-        $(".registration_form1").validate({
-            rules: {
-                "reciever_id": "required",
-                "subject": "required",
-            },
-            submitHandler: function(form) {
-                form.submit();
-            }
-        });
+$(document).ready(function() {
+    $('select').niceSelect();
+    $(".registration_form1").validate({
+        rules: {
+            "reciever_id": "required",
+            "subject": "required",
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
     });
+});
+
 </script>
